d125182e30c9538c7eac31f0b25e78fb5cca0e56
Syd Lawrence <syd@sydlawrence.com>
2011-01-30 22:27:24 +0000

    implemented google maps demo

--- geolocation.js
+++ geolocation.js
@@ -1,58 +1,44 @@
-// define the variables
-var location_btn, location_div;
+var result, article, map;
 
-// when loaded
 window.onload = function() {
-
-	// get the elements
-	location_btn = document.querySelector("#location_btn");
-	location_div = document.querySelector("#location");
-		
-	// listen for click on button
-	location_btn.onclick = function() {
+	article = document.querySelector('article');
 	
-		// add notification to button
-		location_btn.innerHTML = "Please wait...";
-		location_btn.enabled = false;
-		
-		// error checking
-		if (navigator.geolocation) {
-		
-			// get the current position
- 	 		navigator.geolocation.getCurrentPosition(locate_success, locate_error);
-		
-		} else {
-			
-			// display notice
-  			location_div.innerHTML = 'Your browser does not support navigator.geolocation';
-		};	
-	
-	};
-	
-};
-
-// if successfully located
-function locate_success(location) {
-	
-	// get the information available to us
-	var html = "";
-	for (i in location.coords) {
-		html += i+": "+location.coords[i]+"<br/>";
+	map = document.createElement('div');
+	map.id = 'map';
+  	map.style.height = '400px';
+  	map.style.width = '500px';
+  	
+  	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(success, error);
+	} else {
+	  alert('navigator.geolocation not supported on your browser.');
 	}
-	
-	// display found information
-	location_div.innerHTML = html;
-	
-	// reset button
-	location_btn.innerHTML = 'Update my location';
-	location_btn.enabled = true;
-
 }
 
-// if unsuccessfully located
-function locate_error() {
 
-	// display error message
-	location_div.innerHTML = 'There has been an error getting your location';
-	
+function success(position) {
+  
+
+  article.appendChild(map);
+  
+  var ll = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var myOptions = {
+    zoom: 15,
+    center: ll,
+    mapTypeControl: false,
+    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), myOptions);
+  
+  var marker = new google.maps.Marker({
+      position: ll, 
+      map: map, 
+      title:"here you are"
+  });
 }
+
+function error(msg) {
+	article.innerHTML = "We could not find you, sorry.";
+}
+
